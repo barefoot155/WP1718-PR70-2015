cc67e772f34646892aa577f46d3140d0fbca1c4b
Bosa <bosabratic@yahoo.com>
2018-06-27 18:41:53 +0200

    Citanje iz sesije - ispravljeno

--- WebAPI/WebAPI/Controllers/DefaultController.cs
+++ WebAPI/WebAPI/Controllers/DefaultController.cs
@@ -14,55 +14,62 @@ namespace WebAPI.Controllers
         [HttpGet, Route("")]
         public RedirectResult Index()
         {
-            var cookie = Request.Headers.GetCookies("mojKuki").FirstOrDefault();
+            
             var requestUri = Request.RequestUri;
-            
-            if (cookie == null)
+            Korisnik kSesija = (Korisnik)System.Web.HttpContext.Current.Session["mojaSesija"];
+            if (kSesija != null)
             {
-                return Redirect(requestUri.AbsoluteUri + "Login.html");
+                return Redirect(requestUri.AbsoluteUri + kSesija.Uloga.ToString() + ".html");
             }
-            else//zavisi od toga koji je korisnik
+            else
             {
-                string imeKukija = cookie["mojKuki"].Value;//dobijamo username
-                Korisnik korisnik;
-                if ((Korisnici.ListaDispecera.FirstOrDefault(dispecer => dispecer.KorisnickoIme == imeKukija)) != null)
+                var cookie = Request.Headers.GetCookies("mojKuki").FirstOrDefault();
+                if (cookie == null)
                 {
-                    korisnik = Korisnici.ListaDispecera.FirstOrDefault(dispecer => dispecer.KorisnickoIme == imeKukija);
+                    return Redirect(requestUri.AbsoluteUri + "Login.html");
+                }
+                else//zavisi od toga koji je korisnik
+                {
+                    string imeKukija = cookie["mojKuki"].Value;//dobijamo username
+                    Korisnik korisnik;
+                    if ((Korisnici.ListaDispecera.FirstOrDefault(dispecer => dispecer.KorisnickoIme == imeKukija)) != null)
+                    {
+                        korisnik = Korisnici.ListaDispecera.FirstOrDefault(dispecer => dispecer.KorisnickoIme == imeKukija);
 
-                    //HttpCookie httpCookie = new HttpCookie("mojKuki");
-                    //HttpCookie myCookie = Request.Cookies["mojCookie"];
-                    //System.Web.HttpContext.Current.SetSessionStateBehavior(System.Web.SessionState.SessionStateBehavior.Required);
-                    //System.Web.HttpContext.Current.Session["mojaSesija"] = new Dispecer();
-                    System.Web.HttpContext.Current.Session["mojaSesija"] = (Dispecer)korisnik;
-                    return Redirect(requestUri.AbsoluteUri + "Dispecer.html");
-                    //return korisnik.Uloga.ToString();
-                }
-                else if ((Korisnici.ListaVozaca.FirstOrDefault(vozac => vozac.KorisnickoIme == imeKukija)) != null)
-                {
-                    korisnik = Korisnici.ListaVozaca.FirstOrDefault(vozac => vozac.KorisnickoIme == imeKukija);
-                    //System.Web.HttpContext.Current.Session["mojaSesija"] = new Vozac();
-                    System.Web.HttpContext.Current.Session["mojaSesija"] = korisnik;
-                    //return korisnik.Uloga.ToString();
-                    return Redirect(requestUri.AbsoluteUri + "Vozac.html");
-                }
-                else if ((Korisnici.ListaMusterija.FirstOrDefault(musterija => musterija.KorisnickoIme == imeKukija)) != null)
-                {
-                    korisnik = Korisnici.ListaMusterija.FirstOrDefault(musterija => musterija.KorisnickoIme == imeKukija);
-                    //System.Web.HttpContext.Current.Session["mojaSesija"] = new Musterija();
-                    System.Web.HttpContext.Current.Session["mojaSesija"] = korisnik;
-                    return Redirect(requestUri.AbsoluteUri + "Musterija.html");
-                    //return korisnik.Uloga.ToString();
-                }
-                else
-                {
-                    //null je - ne postoji u listama, mozda je u medjuvremenu izbrisan
-                    //return "Pogresno korisnicko ime ili lozinka";
-                }
-                System.Web.HttpContext.Current.Session["mojaSesija"] = null;
-                return Redirect(requestUri.AbsoluteUri + "Login.html");
+                        //HttpCookie httpCookie = new HttpCookie("mojKuki");
+                        //HttpCookie myCookie = Request.Cookies["mojCookie"];
+                        //System.Web.HttpContext.Current.SetSessionStateBehavior(System.Web.SessionState.SessionStateBehavior.Required);
+                        //System.Web.HttpContext.Current.Session["mojaSesija"] = new Dispecer();
+                        System.Web.HttpContext.Current.Session["mojaSesija"] = (Dispecer)korisnik;
+                        return Redirect(requestUri.AbsoluteUri + "Dispecer.html");
+                        //return korisnik.Uloga.ToString();
+                    }
+                    else if ((Korisnici.ListaVozaca.FirstOrDefault(vozac => vozac.KorisnickoIme == imeKukija)) != null)
+                    {
+                        korisnik = Korisnici.ListaVozaca.FirstOrDefault(vozac => vozac.KorisnickoIme == imeKukija);
+                        //System.Web.HttpContext.Current.Session["mojaSesija"] = new Vozac();
+                        System.Web.HttpContext.Current.Session["mojaSesija"] = korisnik;
+                        //return korisnik.Uloga.ToString();
+                        return Redirect(requestUri.AbsoluteUri + "Vozac.html");
+                    }
+                    else if ((Korisnici.ListaMusterija.FirstOrDefault(musterija => musterija.KorisnickoIme == imeKukija)) != null)
+                    {
+                        korisnik = Korisnici.ListaMusterija.FirstOrDefault(musterija => musterija.KorisnickoIme == imeKukija);
+                        //System.Web.HttpContext.Current.Session["mojaSesija"] = new Musterija();
+                        System.Web.HttpContext.Current.Session["mojaSesija"] = korisnik;
+                        return Redirect(requestUri.AbsoluteUri + "Musterija.html");
+                        //return korisnik.Uloga.ToString();
+                    }
+                    else
+                    {
+                        //null je - ne postoji u listama, mozda je u medjuvremenu izbrisan
+                        //return "Pogresno korisnicko ime ili lozinka";
+                    }
+                    System.Web.HttpContext.Current.Session["mojaSesija"] = null;
+                    return Redirect(requestUri.AbsoluteUri + "Login.html");
 
+                }
             }
-            
         }
     }
 }
